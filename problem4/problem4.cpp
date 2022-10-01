0528584d645e248690f86dffda653ced060c1d7e
ahdr3w <andre1qq@icloud.com>
2022-10-01 19:12:14 +0300

    problem4: final realisation

--- problem4/problem4.cpp
+++ problem4/problem4.cpp
@@ -1,13 +1,13 @@
 #include "omp.h"
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
-const int N_1 = 500;
-const int M_1 = 1000;
-const int N_2 = 1000;
-const int M_2 = 700;
 
-int** init_matrix(int N, int M) 
+const int N_1 = 1000; // A[N_1 x M_1]
+const int M_1 = 2000; //
+const int N_2 = 2000; // B[N_2 x M_2]
+const int M_2 = 1500; //
+
+int** init_matrix(int N, int M) // init of random marix A and B
 {
     int** matrix = new int*[N];
     for (int i = 0; i < N; i++)
@@ -18,7 +18,7 @@ int** init_matrix(int N, int M)
     }
     return matrix;
 }
-int** init_zeros(int N, int M) 
+int** init_zeros(int N, int M) // init of matrix C[N_1 x M_2]
 {
     int** matrix = new int*[N];
     for (int i = 0; i < N; i++)
@@ -29,6 +29,11 @@ int** init_zeros(int N, int M)
     }
     return matrix;
 }
+void del(int** matrix, int N) {
+    for (int i = 0; i < N; ++i)
+        delete[] matrix[i];
+    delete[] matrix;
+}
 
 int main() 
 {
@@ -38,16 +43,22 @@ int main()
     int** A = init_matrix(N_1, M_1);
     int** B = init_matrix(N_2, M_2);
     int** C = init_zeros(N_1, M_2);
-    int sum = 0;
-    #pragma omp prallel shared(A, B, C, sum)
-    for (int k = 0; k < M_2; k++)    
-        
-        for (int i = 0; i < N_1; i++) 
+    double t1 = omp_get_wtime();
+    // linear algorithm needs 13.5s
+    #pragma omp parallel for shared(A, B, C) schedule(dynamic) //3.7s working
+    for (int k = 0; k < M_2; k++) //walking on columns of matrix B
+        for (int i = 0; i < N_1; i++) //for each column of matrix B walking on lines of matrix A
         {
-            
-            #pragma omp for reduction(+:sum)
-            for (int j = 0; j < M_1; j++)
+            int sum = 0;
+            //#pragma omp shared(sum)
+            //#pragma omp parallel for schedule(dynamic) reduction(+:sum) // ~158s working
+            for (int j = 0; j < M_1; j++)   //calculating [i, k] element's value of matrix C
                 sum += A[i][j] * B[j][k];  
             C[i][k] = sum;
         }
+    
+    std::cout << "Work time: " << omp_get_wtime() - t1 << std::endl;
+    del(A, N_1);
+    del(B, N_2);
+    del(C, N_1);
 }
