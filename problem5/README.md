790254acba6c0372ad972a16d58b680f96d07d3b
ahdr3w <andre1qq@icloud.com>
2022-10-06 20:50:06 +0300

    problem5: added func for calculating exp

--- problem5/README.md
+++ problem5/README.md
@@ -1,6 +1,10 @@
-Numerical calculation of pi number
+Numerical calculation of pi and exp numbers
 Realistaion with an additional condition
 
 COMMANDS:
 
-mkdir CMake; cd CMake; cmake -G "Unix Makefiles" ..; make; cd ..; OMP_NUM_THREADS=8 ./problem5 100000000000
+mkdir CMake; cd CMake; cmake -G "Unix Makefiles" ..; make; cd ..; OMP_NUM_THREADS=8 ./problem5 100000000000 100000
+
+First number is for pi calculation
+
+Second number is for exp calculation
--- problem5/problem5.cpp
+++ problem5/problem5.cpp
@@ -1,12 +1,12 @@
 #include "omp.h"
 #include <iostream>
 #include <math.h>
-double coeff(int n, double prev_coeff)
+double coeff(int n, double prev_coeff) //func is used in pi_linear
 {
     return prev_coeff * (0.5 - n + 1) / n * (-1);
 }
-
-int main(int argc, char** argv) {
+void pi_linear()
+{
     double cff = 1;
     double x = 0.5;
     double pi = 0.5;
@@ -21,12 +21,14 @@ int main(int argc, char** argv) {
                                //precision - 14 signes after dot, n = 20. 
                               //So there is no reason to use parallel algorithm,
                               //thanks to Newton 
+}
 
-    unsigned long long n = atoi(argv[1]);//number of segments
-    pi = 0;
+void pi_parallel(unsigned long long n)
+{
+    double pi = 0;
     double h = 1. / n; //integration step
     
-    double t1 = omp_get_wtime();
+    
     #pragma omp parallel firstprivate(h, n)
     {    
         #pragma omp for schedule(dynamic, 1000) reduction(+:pi)
@@ -37,5 +39,37 @@ int main(int argc, char** argv) {
     }
     pi *= 4;
     printf("pi = %.14f \n", pi);
-    printf("Time of work: %f \n", omp_get_wtime() - t1);
+}
+
+void exp_parallel(unsigned long long n)
+{
+    double exp = 0;
+    double val = 1;
+    #pragma omp parallel firstprivate(val)
+    {    
+        #pragma omp for schedule(dynamic, 1000) reduction(+: exp)
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= i; ++j) //calculating i!
+                val /= j;
+            exp += val; // 1/1! + 1/2! + ... + 1/(n-1)!
+            val = 1;
+        }
+    }   
+    printf("exp = %.13f\n", exp);
+}
+
+
+int main(int argc, char** argv) {
+    unsigned long long n1 = atoi(argv[1]);//number of segments(for pi)
+    unsigned long long n2 = atoi(argv[2]);//number of iterations(for exp)
+    pi_linear();
+
+    double t1 = omp_get_wtime();
+    pi_parallel(n1);
+    printf("Time of work on calculating pi: %f \n", omp_get_wtime() - t1);
+
+    double t2 = omp_get_wtime();
+    exp_parallel(n2);
+    printf("Time of work on calculating exp: %f \n", omp_get_wtime() - t2);
 }
